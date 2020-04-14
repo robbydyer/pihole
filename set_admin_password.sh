f9aac2b2abdc218135ec393d5e8d357f1cd849e7
root <robby.dyer@gmail.com>
2020-04-14 03:25:56 +0100

    wip

--- set_admin_password.sh
+++ set_admin_password.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+set -euo pipefail
 
-docker exec -it pihole pihole -a -p "$1"
+ROOT="$(git rev-parse --show-toplevel)"
+PASSFILE="${ROOT}/.webpass"
+
+if [ -n "${1:-}" ]; then
+  pass="$1"
+elif [ -f "${PASSFILE}" ]; then
+  pass="$(cat "${PASSFILE}")"
+else
+  echo "Please pass a password as arg, or set password in ${PASSFILE}"
+  exit 1
+fi
+
+docker exec -it pihole pihole -a -p "${pass}"
